b20c6cccc14fdf1f5049403d4f33eec77a5104d6
miscbits <willow6786@gmail.com>
2019-04-15 21:51:00 -0400

    trying development mode routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   resources :jobs, only: [:index, :show]
   resources :tips, only: [:index, :show]
   resources :reports, only: [:index, :show]
+
+  if Rails.env.development?
+    resources :decks, except: [:index, :show]
+    resources :documents, except: [:index, :show]
+    resources :jobs, except: [:index, :show]
+    resources :tips, except: [:index, :show]
+    resources :reports, except: [:index, :show]
+  end
 end
